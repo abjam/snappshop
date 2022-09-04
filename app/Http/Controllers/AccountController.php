953482b94a7dd42a9556d7fd29506143bc74202e
Alex <Alex@gmail.com>
2022-09-04 21:02:35 +0430

    Build transaction without validation

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Card;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Transaction;
@@ -40,5 +41,57 @@ class AccountController extends Controller
             ->get();
 
         return response()->json($users_info->toArray());
+    }
+
+    /**
+     * Make transaction and update balance of account
+     *
+     * @param  mixed $request
+     *
+     * @return JSON
+     */
+    public function makeTransaction(Request $request) {
+
+        $validatedData = $request->validate([
+            'from' => ['required', 'numeric', 'min:16', 'max:16'],
+            'to' => ['required', 'min:16', 'numeric', 'max:16'],
+            'price' => ['required', 'numeric'],
+            'card_number' => ['required', 'numeric'],
+            'card_id' => ['required', 'numeric'],
+        ]);
+
+        if($validatedData) {
+
+            $card_info = Card::find($request->input($request->input('card_id')));
+            $balance = $card_info->price - $card_info->price;
+
+            if($balance > 0) {
+
+                $result = $card_info->update(['price' => $balance]);
+
+                if($result) {
+                    $trans = new Transaction;
+                    $trans->card_id = $request->card_id;
+                    $trans->card_number = $request->card_number;
+                    $trans->from = $request->from;
+                    $trans->to = $request->to;
+                    $trans->save();
+
+                    if($trans) {
+                        return response()->json($trans->toArray(),
+                            ['message' => 'تراکنش با موفقیت انجام شد.']);
+                    } else {
+                        return response()->json([
+                            'message' => 'ثبت تراکنش با شکست مواجه شد.'
+                        ]);
+                    }
+                }
+
+            } else {
+                return response()->json([
+                    'message' => 'موجودی کافی نیست.'
+                ]);
+            }
+        }
     }
 }
